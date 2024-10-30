134ed352200ec1b55993b2a9f56f6767f134cd87
Yulika19 <ulika14.02@mail.ru>
2024-10-30 23:32:01 +0300

    Add for lab2

--- second-service/src/main/java/org/secondservice/controller/AgencyController.java
+++ second-service/src/main/java/org/secondservice/controller/AgencyController.java
@@ -2,7 +2,6 @@ package org.secondservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.secondservice.service.AgencyService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
